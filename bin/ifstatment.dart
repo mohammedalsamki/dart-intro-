b55c19f171a88db1e410d80ced2ff4edfe7737de
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2023-04-29 10:56:25 +0300

    added while and do while loop

--- bin/ifstatment.dart
+++ bin/ifstatment.dart
@@ -27,7 +27,7 @@ void main() {
   // print list
 
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-// insted of using the normal print we can use something called for loop
+// insted  of using the normal print we can use something called for loop
 
   for (var i = 0; i < numbers.length; i++) {
     print(numbers[i]);
@@ -38,4 +38,61 @@ void main() {
   for (var j = 0; j < 7; j++) {
     print(j);
   }
+
+  // inline if statement
+
+  int x = 10;
+  int y = 10;
+
+  print('------' * 15);
+
+  if (x > y) {
+    print('x is greater than y');
+  } else {
+    print('x is less than y');
+  }
+//        false >> 'x is less than y'
+  print('condition is true');
+
+  //         true >> 'x is greater than y'
+  //         false >> 'x is less than y'
+  var z = x > y ? 'x is greater than y' : 'x is less than y';
+  print(z);
+
+  //   true >> 'x is less than y'
+  //   false >> 'x is greater than y'
+  print('condition is false');
+  //  x> y is false >> 10 > 10 >> false >> we added ! to make it true
+  var n = !(x > y) ? 'x is greater than y' : 'x is less than y';
+  print(n);
+
+  print('------' * 15);
+
+  // loops in dart
+
+  // for loop
+
+  // while loop
+
+  // break and continue
+
+  // while (x < 10) {
+  //   print('hello');
+  //   x++;
+  //   if (x == 8) {
+  //     break;
+  //   }
+  // }
+
+  print('------' * 15);
+
+  // do while loop
+// do while loop will run the code first and then check the condition
+  do {
+    print('hello');
+    print(x);
+    x++;
+  } while (x < 9);
+
+  print('------' * 15);
 }
