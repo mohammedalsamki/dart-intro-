3ddbb806562fc1f4903de23ee5abd8aac8d05c3a
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2023-05-02 13:39:50 +0300

    review

--- bin/test.dart
+++ bin/test.dart
@@ -3,23 +3,61 @@ void main() {
   String t = 'Turkish';
   String e = 'Espresso';
   String a = 'American coffee';
-  int code = 5;
-
-  if (code == 1) {
-    print(t);
-  } else if (code == 2) {
-    print(e);
-  } else if (code == 3) {
-    print(a);
-  } else {
-    print('invalid value ');
-  }
-
-
+  int code = 2;
 
   // -------------------
 
-  if (code >=1 && code <= 4) {
-    
+  // if (code >= 1 && code <= 4) {
+  //   if (code == 1) {
+  //     print(t);
+  //   } else if (code == 2) {
+  //     print(e);
+  //   } else if (code == 3) {
+  //     print(a);
+  //   } else {
+  //     print('invalid value ');
+  //   }
+  // }
+  // switch
+
+  // switch (code) {
+  //   case 1:
+  //     print(t);
+  //     break;
+  //   case 2:
+  //     print(e);
+  //     break;
+  //   case 3:
+  //     print(a);
+  //     break;
+  //   default:
+  //     print('invalid value ');
+  // }
+  // for
+
+  // List > data type > name = [index[0],index[1],index[2],index[3],index[4],index[5],index[6]]
+  List numbers = [1, 5, 8, 6, 7, 2, 11, 20];
+
+  // for (var i = 0; i < numbers.length; i++) {
+  //   print(numbers[i]);
+  // }
+  int contes = 1;
+  // while loop
+
+  do {
+    print(contes);
+    contes++;
+  } while (contes < 7 && contes > 1);
+  {
+    if (contes == 5) {
+    } else {
+      print(contes);
+    }
+    contes++;
   }
+  // do while loop
+
+//   do {
+
+//   } while (condition);
 }
