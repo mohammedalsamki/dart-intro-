81244a9e19d0bbadb36f89038d06c66b18a8fe82
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2023-05-02 12:44:49 +0300

    add if new

---
+++ bin/test.dart
@@ -0,0 +1,25 @@
+void main() {
+  // if statment
+  String t = 'Turkish';
+  String e = 'Espresso';
+  String a = 'American coffee';
+  int code = 5;
+
+  if (code == 1) {
+    print(t);
+  } else if (code == 2) {
+    print(e);
+  } else if (code == 3) {
+    print(a);
+  } else {
+    print('invalid value ');
+  }
+
+
+
+  // -------------------
+
+  if (code >=1 && code <= 4) {
+    
+  }
+}
