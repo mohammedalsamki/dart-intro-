8fed1da8b830989879b52cbbe998590c5cc5dc6c
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2023-05-04 10:57:03 +0300

    added method topic to our intro

---
+++ bin/methods.dart
@@ -0,0 +1,34 @@
+void main() {
+  // params and return
+  // void
+  // data type return type
+
+  // void methodname(){
+  //   // code
+  // }
+
+  // void printName() {
+  //   print('mohammad');
+  //   print('lutfy');
+  //   print('hasan');
+  // }
+
+  // printName();
+
+// method to calculate the sum of 2 numbers
+
+  dynamic sum({var a, var b, var c, var d, var e}) {
+    var z = a + b;
+    return z;
+  }
+
+  print(sum(
+    a: 5,
+    c: 6,
+    d: 7,
+    e: 8,
+    b: 10,
+  ));
+
+  // 2 methods >> 1. return string 2. return int
+}
