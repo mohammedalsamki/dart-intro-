e7cf057adf2e144aa6cabe21a70290ac52810d84
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2023-05-07 13:46:21 +0300

    we review the method and functions in dart

--- bin/methods.dart
+++ bin/methods.dart
@@ -22,13 +22,54 @@ void main() {
     return z;
   }
 
-  print(sum(
-    a: 5,
-    c: 6,
-    d: 7,
-    e: 8,
-    b: 10,
-  ));
+  // print(sum(
+  //   a: 5,
+  //   c: 6,
+  //   d: 7,
+  //   e: 8,
+  //   b: 10,
+  // ));
 
   // 2 methods >> 1. return string 2. return int
+
+  // declare method
+  void devideTwoNumbers() {
+    // code
+    var a = 5;
+    var b = 10;
+    var c = a / b;
+    return print(c);
+  }
+// is and is!
+
+var x=5.0;
+if (x is! int){
+  // print type of x
+  print(x.runtimeType);
+
+  // print();
+}
+else{
+  print('yes x is int');
+}
+  double devideTwoNumbersDouble(
+      {var d, int e = 0, required int a, required int b}) {
+    var c=0.0;
+    if (a > b) {
+      c = a / b;
+    } else {
+     print('not valied');
+    }
+
+    return c;
+  }
+
+  print(devideTwoNumbersDouble(a: 6, b: 25, d: 5, e: 6));
+  // print(devideTwoNumbersDouble(a:6, b:25));
+  // devideTwoNumbers();
+
+  // method to print name
+
+  // declare a method to calculate the sum of 2 numbers which are salary and bonus also with employee name
+  // return employee name and salary and bonus and total salary
 }
