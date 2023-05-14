12490be5ec8acda453914aae45dc7e623f70eed5
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2023-05-14 13:14:53 +0300

    add privet

---
+++ bin/privet.dart
@@ -0,0 +1,62 @@
+
+class Vicles{
+
+  String? _model;
+  int? _year;
+
+  // getter and setter
+  // getter
+  String? get model => _model;
+
+  // setter
+
+  set model(String? model)=> _model = model;
+
+  // getter
+
+  int? get year => _year;
+
+  // setter
+
+  set year(int? year)=> _year = year;
+
+}
+
+// declare class for employee will contain name, rol, experience, salary, bonus, and total salary bit the salary will be privet and the bonus will be privet
+
+class Employee{
+  String? name;
+  int? rol;
+  String? experience;
+  int _salary=300;
+  int? bonus;
+  int? _totalSalary;
+  // add _salary and bouns to the totalSalary
+
+  // getter and setter
+  // getter
+  int? totlSalary => totalSalary;
+
+}
+void main(){
+  // create a privet var in dart with classes
+  // var Vicle1 = Vicles();
+  // Vicle1.model = "BMW";
+  // Vicle1.year = 2021;
+  // print(Vicle1.model);
+  // print(Vicle1.year);
+
+  // creat object from employee class
+  var employee1 = Employee();
+  employee1.name = "ali";
+  employee1.rol = 1;
+  employee1.experience = "2 years";
+  employee1.bonus = 100;
+  employee1.totalSalary = employee1._salary! + employee1.bonus!;
+  print(employee1.name);
+  print(employee1.rol);
+  print(employee1.experience);
+  print(employee1.totalSalary);
+
+  
+}
